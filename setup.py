77417854f890e69edcb1f95cd8bc13258ac0cf7d
Michael Weedon <michael.weedon@apitvista.com>
2018-12-13 14:50:52 -0500

    modifications to layout

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gsk-pydb-example',
+    version='0.9',
+    packages=[''],
+    url='',
+    license='',
+    author='mw330057',
+    author_email='',
+    description=''
+)
